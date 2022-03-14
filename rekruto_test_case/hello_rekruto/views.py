9e8e93b29f9511fa1f0482ee74083cd9f7c8f891
Lev Sokolov <Lev.Sokolov.01@mail.ru>
2022-03-15 02:56:40 +0300

    Hello page functional added

--- rekruto_test_case/hello_rekruto/views.py
+++ rekruto_test_case/hello_rekruto/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def greet(request):
+    name = request.GET.get('name', '')
+    message = request.GET.get('message', '')
+
+    context = {
+        'name': name,
+        'message': message
+    }
+    print(context)
+
+    return render(request, 'greeter/greeter.html', context)
